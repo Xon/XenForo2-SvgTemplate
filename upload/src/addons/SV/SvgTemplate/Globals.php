fc1c3ba69a2d24cbf7a70dea3af3fa9ac5ffec0d
Xon <635541+Xon@users.noreply.github.com>
2023-03-20 21:11:28 +0800

    Document why Globals::templateHelper is the mess it is

--- upload/src/addons/SV/SvgTemplate/Globals.php
+++ upload/src/addons/SV/SvgTemplate/Globals.php
@@ -4,23 +4,28 @@ namespace SV\SvgTemplate;
 
 use SV\StandardLib\TemplaterHelper;
 use SV\SvgTemplate\SV\StandardLib\TemplaterHelper as ExtendedTemplaterHelper;
+use XF\Template\Templater;
 
 /**
  * Class Globals
  *
  * @package SV\SvgTemplate
  */
-class Globals
+abstract class Globals
 {
-    // NOT USED. Exists to ensure upgrades from older versions do not cause errors
-    public static $templater = null;
+    private function __construct() { }
 
     /**
-     * @return ExtendedTemplaterHelper
+     * NOT USED. Exists to ensure upgrades from older versions do not cause errors
+     * @deprecated
      */
-    public static function templateHelper(\XF\Template\Templater $templater): TemplaterHelper
+    public static $templater = null;
+
+    public static function templateHelper(Templater $templater): ExtendedTemplaterHelper
     {
         // Note; TemplaterHelper MUST NOT be the extended version!!!
+        // The add-on will be in an effective zombie state so the XFCP parts will not be resolved
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
         return TemplaterHelper::get($templater);
     }
 }
