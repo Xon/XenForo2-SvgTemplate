1e9db7cb55bfd23ab25cd3e5e8a7a258d587c015
Xon <635541+Xon@users.noreply.github.com>
2023-09-30 15:34:23 +0800

    Add return type hints & simplify svgWriter::getResponse

--- upload/src/addons/SV/SvgTemplate/svgWriter.php
+++ upload/src/addons/SV/SvgTemplate/svgWriter.php
@@ -7,21 +7,22 @@ namespace SV\SvgTemplate;
 
 use SV\SvgTemplate\Repository\Svg as SvgRepo;
 use XF\App as BaseApp;
-use XF\CssRenderer;
 use XF\CssWriter;
 use XF\Http\ResponseStream;
 use XF\Http\Response;
 use function strpos, gzdecode, md5;
 
+/**
+ * @property svgRenderer $renderer
+ */
 class svgWriter extends CssWriter
 {
     /** @var int */
-    const PNG_CACHE_TIME = 3600; // 1 hour
+    public const PNG_CACHE_TIME = 3600; // 1 hour
 
-    public function run(array $templates, $styleId, $languageId, $validation = null, int $date = null)
+    public function run(array $templates, $styleId, $languageId, $validation = null, int $date = null): Response
     {
         $request = \XF::app()->request();
-        /** @var svgRenderer $renderer */
         $renderer = $this->renderer;
 
         if ($date !== null)
@@ -38,29 +39,27 @@ class svgWriter extends CssWriter
         return parent::run($templates, $styleId, $languageId, $validation);
     }
 
-    /**
-     * @return CssRenderer|svgRenderer
-     */
     public function getRenderer() : svgRenderer
     {
         return $this->renderer;
     }
 
-    /**
-     * @return bool
-     */
     public function isRenderingPng() : bool
     {
         return $this->getRenderer()->isRenderingPng();
     }
 
-    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * @param string|Response $output
+     * @return string|Response
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
     public function finalizeOutput($output)
     {
         return $output;
     }
 
-    protected function setupResponse(Response $response) : Response
+    protected function setupResponse(Response $response): Response
     {
         if ($this->isRenderingPng())
         {
@@ -77,6 +76,10 @@ class svgWriter extends CssWriter
         return $response;
     }
 
+    /**
+     * @param string|Response $output
+     * @return string|Response
+     */
     public function getResponse($output)
     {
         $response = parent::getResponse($output);
@@ -101,60 +104,63 @@ class svgWriter extends CssWriter
                 }
             }
         }
-        else if ($output)
+        else if (is_string($output) && $output !== '')
         {
             $hasOutput = true;
         }
 
-        if ($hasOutput)
+        if (!$hasOutput)
         {
-            if ($this->isRenderingPng())
+            $response->body('');
+            $response->httpCode(404);
+
+            return '';
+        }
+
+        if (!$this->isRenderingPng())
+        {
+            return $response;
+        }
+
+        /** @noinspection PhpConditionAlreadyCheckedInspection */
+        if ($output instanceof ResponseStream)
+        {
+            $output = gzdecode($output->getContents());
+        }
+
+        $cacheKey = $cacheObj = $img = null;
+        $caching = $this->app()->options()->svSvgTemplate_cacheRenderedSvg ?? false;
+        if ($caching)
+        {
+            $cacheKey = 'svSvg_Png_' . md5($output);
+            $cacheObj = $this->app()->cache('sv-svg-img', false);
+            if (!$cacheObj)
             {
-                if ($output instanceof ResponseStream)
-                {
-                    $output = gzdecode($output->getContents());
-                }
-
-                $cacheKey = $cacheObj = $img = null;
-                $caching = $this->app()->options()->svSvgTemplate_cacheRenderedSvg ?? false;
-                if ($caching)
-                {
-                    $cacheKey = 'svSvg_Png_' . md5($output);
-                    $cacheObj = $this->app()->cache('sv-svg-img', false);
-                    if (!$cacheObj)
-                    {
-                        $cacheObj = $this->app()->cache('css', false);
-                    }
-                }
-
-                if ($cacheObj)
-                {
-                    $img = $cacheObj->fetch($cacheKey);
-                }
-
-                if (!$img)
-                {
-                    /** @var SvgRepo $svgRepo */
-                    $svgRepo = \XF::repository('SV\SvgTemplate:Svg');
-                    $img = $svgRepo->convertSvg2Png($output);
-
-                    if ($cacheObj)
-                    {
-                        $cacheObj->save($cacheKey, $img, static::PNG_CACHE_TIME);
-                    }
-                }
-
-                if ($img)
-                {
-                    $response->compressIfAble(false);
-                    $response->body($img);
-                }
-                else
-                {
-                    $response->body('');
-                    $response->httpCode(404);
-                }
+                $cacheObj = $this->app()->cache('css', false);
             }
+        }
+
+        if ($cacheObj)
+        {
+            $img = $cacheObj->fetch($cacheKey);
+        }
+
+        if (!$img)
+        {
+            /** @var SvgRepo $svgRepo */
+            $svgRepo = \XF::repository('SV\SvgTemplate:Svg');
+            $img = $svgRepo->convertSvg2Png($output);
+
+            if ($cacheObj)
+            {
+                $cacheObj->save($cacheKey, $img, static::PNG_CACHE_TIME);
+            }
+        }
+
+        if ($img)
+        {
+            $response->compressIfAble(false);
+            $response->body($img);
         }
         else
         {
@@ -175,9 +181,6 @@ class svgWriter extends CssWriter
         return $response;
     }
 
-    /**
-     * @return BaseApp
-     */
     protected function app() : BaseApp
     {
         return $this->app;
