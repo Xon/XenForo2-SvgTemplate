b7663a43f1c1070093ddc42fec98b31ee13b4ceb
Xon <635541+Xon@users.noreply.github.com>
2025-05-05 23:23:30 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/SvgTemplate/_output/option_hint.php
@@ -0,0 +1,20 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $svSvgTemplateRouterIntegration Support SVG url's in XF routing layer
+ * @property bool|null $svSvgTemplate_cacheRenderedSvg Cache SVG which are rendered to static images
+ * @property array|null $svSvgTemplate_renderSvgAsPng Render SVG's as PNG for mobile devices
+ */
+class Options
+{
+}
